94eed9212d6c47343d158086ff32d7126741a536
Drawdecais <erickdavid1933@gmail.com>
2022-03-24 00:11:54 -0400

    Agregar

---
+++ Hola.py
@@ -0,0 +1,12 @@
+<<<<<<< HEAD
+#!/usr/bin/python3
+#Declaro una variable
+vNombreCompleto = "Erick David Benitez Perez"
+#Imprimo la variable en pantalla
+=======
+#!/usr/bin/python3
+#Declaro una variable
+vNombreCompleto = "Erick David Benitez Perez"
+#Imprimo la variable en pantalla
+>>>>>>> 59fb4ac (Agregar)
+print("Hola Prog1, mi nombre es", vNombreCompleto)
